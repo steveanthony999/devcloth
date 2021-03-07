fa4341b4d03961ec0564c5f8482c7e0fdc5dadd6
Steven Woodward <steveanthony999@gmail.com>
2021-03-07 13:50:10 -0800

    made mobile navbar and made responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,6 +5,7 @@ import {
   faUserAlt,
   faHeart,
   faShoppingBag,
+  faBars,
 } from '@fortawesome/free-solid-svg-icons';
 
 import './navbar.scss';
@@ -12,37 +13,45 @@ import Logo from '../../images/logo.png';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className='upper-nav util-border-bottom'>
-        <div className='util-container util-flex-center-between util-py-1 util-extra-small-heading'>
-          <span>Free shipping on all orders over $99!</span>
-          <FontAwesomeIcon icon={faSearch} />
+    <>
+      <nav className='main-nav'>
+        <div className='upper-nav util-border-bottom'>
+          <div className='util-container util-flex-center-between util-py-1 util-extra-small-heading'>
+            <span>Free shipping on all orders over $99!</span>
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
         </div>
-      </div>
-      <div className='middle-nav util-py-4 util-border-bottom'>
+        <div className='middle-nav util-py-4 util-border-bottom'>
+          <img src={Logo} alt='logo' />
+        </div>
+        <div className='lower-nav util-border-bottom'>
+          <div className='util-container util-flex-center-between util-py-1'>
+            <div>
+              <NavLink to='/' className='util-mr-2'>
+                Home
+              </NavLink>
+              <NavLink to='/' className='util-mr-2'>
+                Blog
+              </NavLink>
+              <NavLink to='/' className='util-mr-2'>
+                About Us
+              </NavLink>
+            </div>
+            <div className='bottom-nav'>
+              <FontAwesomeIcon className='util-ml-2' icon={faUserAlt} />
+              <FontAwesomeIcon className='util-ml-2' icon={faHeart} />
+              <FontAwesomeIcon className='util-ml-2' icon={faShoppingBag} />
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <nav className='mobile-nav'>
+        <FontAwesomeIcon icon={faBars} />
         <img src={Logo} alt='logo' />
-      </div>
-      <div className='lower-nav util-border-bottom'>
-        <div className='util-container util-flex-center-between util-py-1'>
-          <div>
-            <NavLink to='/' className='util-mr-2'>
-              Home
-            </NavLink>
-            <NavLink to='/' className='util-mr-2'>
-              Blog
-            </NavLink>
-            <NavLink to='/' className='util-mr-2'>
-              About Us
-            </NavLink>
-          </div>
-          <div>
-            <FontAwesomeIcon className='util-ml-2' icon={faUserAlt} />
-            <FontAwesomeIcon className='util-ml-2' icon={faHeart} />
-            <FontAwesomeIcon className='util-ml-2' icon={faShoppingBag} />
-          </div>
-        </div>
-      </div>
-    </nav>
+        <FontAwesomeIcon icon={faSearch} />
+      </nav>
+    </>
   );
 };
 
