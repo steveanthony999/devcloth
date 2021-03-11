298cf3f6c33e179ebb2a0a7866b6fbee021f9bb0
Steven Woodward <steveanthony999@gmail.com>
2021-03-10 22:26:01 -0800

    set up container for shop page

--- src/screens/shopscreen/Shop.js
+++ src/screens/shopscreen/Shop.js
@@ -1,14 +1,18 @@
 import CollectionPreview from '../../components/catalogue/collectionpreview/CollectionPreview';
 
+import './shop.scss';
+
 import SHOP_DATA from './shopData';
 
 const Shop = () => {
   return (
-    <div>
-      {SHOP_DATA.map(({ id, ...rest }) => (
-        <CollectionPreview key={id} {...rest} />
-      ))}
-    </div>
+    <section id='shop'>
+      <div className='shop-container'>
+        {SHOP_DATA.map(({ id, ...rest }) => (
+          <CollectionPreview key={id} {...rest} />
+        ))}
+      </div>
+    </section>
   );
 };
 
