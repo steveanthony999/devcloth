4e4a772412107c62c3634824d9493c9cc2283399
Steven Woodward <steveanthony999@gmail.com>
2021-03-11 21:23:41 -0800

    updated products listings

--- src/screens/shopscreen/shopData.js
+++ src/screens/shopscreen/shopData.js
@@ -117,24 +117,24 @@ const SHOP_DATA = [
   },
   {
     id: 3,
-    title: 'Hoodies',
-    routeName: 'hoodies',
+    title: 'Hoodies & Sweatshirts',
+    routeName: 'sweatshirts',
     items: [
       {
         id: 1,
-        name: 'Black Jean Shearling',
+        name: 'Bios Hoodie',
         imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
         price: 125,
       },
       {
         id: 2,
-        name: 'Blue Jean Jacket',
+        name: 'Bios Unisex Sweatshirt',
         imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
         price: 90,
       },
       {
         id: 3,
-        name: 'Grey Jean Jacket',
+        name: 'Hoodie Sweater',
         imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
         price: 90,
       },
@@ -159,19 +159,19 @@ const SHOP_DATA = [
     items: [
       {
         id: 1,
-        name: 'Blue Tanktop',
+        name: 'Beanie',
         imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
         price: 25,
       },
       {
         id: 2,
-        name: 'Floral Blouse',
+        name: 'Hat',
         imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
         price: 20,
       },
       {
         id: 3,
-        name: 'Floral Dress',
+        name: 'Backpack',
         imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
         price: 80,
       },
