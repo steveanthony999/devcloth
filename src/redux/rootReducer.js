bfd085c9fd814fa95275dde5164ab6071bc79010
Steven Woodward <steveanthony999@gmail.com>
2021-03-22 12:05:54 -0700

    add cart persistence using redux-persist

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,21 @@
 // The actual base reducer object that represents all of the state of our application. Will be the actual code that combines all state together.
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/userReducer';
 import cartReducer from './cart/cartReducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
